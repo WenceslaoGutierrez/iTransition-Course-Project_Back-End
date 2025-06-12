b8730a665323dc9ae986155a4f291003d5abeff4
Wenceslao <wences_gv@hotmail.com>
2025-06-12 14:53:40 -0600

    Basic configuration for i18n.

---
+++ src/config/i18n.config.ts
@@ -0,0 +1,21 @@
+import i18next from 'i18next';
+import Backend from 'i18next-fs-backend';
+import { LanguageDetector } from 'i18next-http-middleware';
+import path from 'path';
+
+i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    preload: ['en', 'es'],
+    backend: {
+      loadPath: path.join(__dirname, '../../locales/{{lng}}/{{ns}}.json')
+    },
+    detection: {
+      order: ['header', 'querystring', 'cookie'],
+      caches: ['cookie']
+    }
+  });
+
+export default i18next;
