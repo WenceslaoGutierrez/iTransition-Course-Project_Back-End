494a5c0654e2c362583f6d5c3ed5592478df9761
Wenceslao <wences_gv@hotmail.com>
2025-06-13 13:38:56 -0600

    Logic for register and auth.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,8 @@
+import { create } from 'domain';
 import { PrismaClient, User } from '../generated/prisma/client';
 import bcrypt from 'bcrypt';
-
 const jwt = require('jsonwebtoken');
+
 const prisma = new PrismaClient();
 
 export const generateToken = (user: User) => {
@@ -25,4 +26,32 @@ export const verifyPassword = async (password: string, hashedPassword: string) =
 export const prepareUserResponse = (user: User) => {
   const { password_hash, ...userWithoutPassword } = user;
   return userWithoutPassword;
+};
+
+export const createUser = async (data: Omit<User, 'id' | 'role' | 'isBlocked'>) => {
+  return await prisma.user.create({ data });
+};
+
+export const findUserByEmail = async (email: string) => {
+  return await prisma.user.findUnique({ where: { email } });
+};
+
+export const registrationProcess = async (userData: Omit<User, 'id' | 'role' | 'isBlocked'>) => {
+  const hashedPassword = await hashPassword(userData.password_hash);
+  const newUser = await createUser({ ...userData, password_hash: hashedPassword });
+  return prepareUserResponse(newUser);
+};
+
+export const authenticateUser = async (credentials: Pick<User, 'email' | 'password_hash'>) => {
+  const user = await findUserByEmail(credentials.email);
+  if (!user) return null;
+  const isPasswordCorrect = await verifyPassword(credentials.password_hash, user.password_hash);
+  if (!isPasswordCorrect) return null;
+  return user;
+};
+
+export const loginResponse = (user: User) => {
+  const token = generateToken(user);
+  const userResponse = prepareUserResponse(user);
+  return { token, user: userResponse };
 };
