0cf5466542de23acabdc69e61e4bb4a679c48d2f
Wenceslao <wences_gv@hotmail.com>
2025-06-21 17:38:27 -0600

    Template routes.

---
+++ src/api/template.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { validate } from '../middleware/genericValidate.middleware';
+import { createTemplateSchema } from '../schemas/template.schema';
+import { protect } from '../middleware/auth.middleware';
+import * as templateController from '../controllers/template.controller';
+
+const router = Router();
+router.use(protect);
