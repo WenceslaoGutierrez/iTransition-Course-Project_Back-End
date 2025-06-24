12c52b3cde6aa106c5530c39e48bddcf229c9746
Wenceslao <wences_gv@hotmail.com>
2025-06-23 19:12:47 -0600

    Template controller updated.

--- src/controllers/template.controller.ts
+++ src/controllers/template.controller.ts
@@ -10,3 +10,16 @@ export const createTemplate = async (req: Request, res: Response) => {
     res.status(500).json({ message: req.t('error.server_error') });
   }
 };
+
+//READ
+
+export const getMyTemplates = async (req: Request, res: Response) => {
+  try {
+    const authorId = req.user!.id;
+    const templates = await templateService.getTemplatesByAuthor(authorId);
+    res.status(200).json(templates);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: req.t('error.server_error') });
+  }
+};
