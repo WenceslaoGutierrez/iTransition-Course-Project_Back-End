e17b008cd27466807b41dc2bc25d038b49d89310
Wenceslao <wences_gv@hotmail.com>
2025-06-25 20:14:03 -0600

    Routes for CRUD in templates.

--- src/api/template.routes.ts
+++ src/api/template.routes.ts
@@ -3,12 +3,17 @@ import { validate } from '../middleware/genericValidate.middleware';
 import { createTemplateSchema } from '../schemas/template.schema';
 import { protect } from '../middleware/auth.middleware';
 import * as templateController from '../controllers/template.controller';
+import { validateAndLoadTemplate } from '../middleware/template.middleware';
 
 const router = Router();
 router.use(protect);
 
-//TO DO
-//CREATE (POST)
-//READ (GET)
-//DELETE()
-//UPDATE()
+router.route('/').post(validate(createTemplateSchema), templateController.createTemplate).get(templateController.getMyTemplates);
+
+router
+  .route('/:id')
+  .get(validateAndLoadTemplate, templateController.getTemplateById)
+  .put(validateAndLoadTemplate, templateController.updateTemplate)
+  .delete(validateAndLoadTemplate, templateController.deleteTemplate);
+
+export default router;
